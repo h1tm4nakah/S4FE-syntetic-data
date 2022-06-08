4f4297d1a54ccc296eef78becba70bb8bf17da87
Pietro Rustici <p.rustici93@gmail.com>
2022-06-08 18:57:26 +0200

    add download

--- src/App.js
+++ src/App.js
@@ -224,7 +224,8 @@ function generateJSON() {
 		}
 		currentTime += interval;
 	}
-	console.log(jsonOutput);
+
+	saveJson(jsonOutput);
 }
 
 function generateAgents() {
@@ -449,6 +450,33 @@ class Camera {
 	getRelativePosition(point) {
 		return [point.x - this.min_x, point.z - this.min_z];
 	}
+}
+
+// TO GENERATE AND DOWNLOAD JSON
+var saveJson = function(obj) {
+	var str = JSON.stringify(obj);
+	var data = encode( str );
+
+	var blob = new Blob( [ data ], {
+		type: 'application/octet-stream'
+	});
+	
+	var url = URL.createObjectURL( blob );
+	var link = document.createElement( 'a' );
+	link.setAttribute( 'href', url );
+	link.setAttribute( 'download', 'data.json' );
+	var event = document.createEvent( 'MouseEvents' );
+	event.initMouseEvent( 'click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+	link.dispatchEvent(event);
+}
+
+
+var encode = function(s) {
+	var out = [];
+	for ( var i = 0; i < s.length; i++ ) {
+		out[i] = s.charCodeAt(i);
+	}
+	return new Uint8Array( out );
 }
 
 export default App;
